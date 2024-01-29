c88cc390d0dea09b799ac99d44fba39a934ee25b
Matthieu Barba <mbarba@ebi.ac.uk>
2024-01-29 16:43:20 +0000

    Fix count init, add test

--- src/python/ensembl/io/genomio/gff3/id_allocator.py
+++ src/python/ensembl/io/genomio/gff3/id_allocator.py
@@ -38,7 +38,11 @@ class IDAllocator:
     current_id_number: int = 0
     make_missing_stable_ids: bool = True
     prefix = "TMP_"
-    _loaded_ids = set()
+    _loaded_ids: set = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._loaded_ids = set()
 
     def generate_id(self) -> str:
         """Returns a new unique gene stable_id with a prefix.
